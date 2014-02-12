56c9265b3cf21e242de8b2f337be672260742c66
Juan Carlos <juanjcsr@gmail.com>
2014-02-12 14:09:50 -0600

    pruebas de layout de pagina principal, fixes #11

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+
+  let(:base_title) { "¡Muévete!" }
   
-  describe "Pagina de inicio" do
+  describe "pagina de inicio" do
     
     it "should have the content '¡Muévete!'" do
       visit '/static_pages/index'
@@ -19,5 +21,34 @@ describe "Static Pages" do
       expect(page).to have_content('Información')
     end
     
+    it "should have the title 'Inicio'" do
+      visit '/static_pages/index'
+      expect(page).to have_title('Inicio')
+    end
+    
+  end
+  
+  describe "pagina de ayuda" do
+    it "should have the content 'Ayuda'" do
+      visit '/static_pages/ayuda'
+      expect(page).to have_content('Ayuda')
+    end
+    
+    it "should have the title 'Ayuda'" do
+      visit '/static_pages/ayuda'
+      expect(page).to have_title("#{base_title} | Ayuda")
+    end
+  end
+  
+  describe "pagina de informacion" do
+    it "should have the content 'Informacion'" do
+      visit '/static_pages/info'
+      expect(page).to have_content('Información')
+    end
+    
+    it "shoud have the title 'Información'" do
+      visit '/static_pages/info'
+      expect(page).to have_title('Información')
+    end
   end
 end
