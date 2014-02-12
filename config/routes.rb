00fbc9ede14eece8940e05e57e52d3a48e289785
Juan Carlos <juanjcsr@gmail.com>
2014-02-12 17:42:52 -0600

    colocar la ruta de inicio a la app

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 SedesaApp::Application.routes.draw do
-  get "static_pages/index"
+  #get "static_pages/index"
   get "static_pages/ayuda"
   get "static_pages/info"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
