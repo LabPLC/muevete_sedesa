4ebe212f6488a553786c098f073d403baadf0fcc
Juan Carlos <juanjcsr@gmail.com>
2014-02-18 16:27:37 -0600

    agregar puntos a modelo d usuario

---
+++ db/migrate/20140218220143_add_puntos_to_users.rb
@@ -0,0 +1,5 @@
+class AddPuntosToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :points, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140217224223) do
+ActiveRecord::Schema.define(version: 20140218220143) do
 
   create_table "posts", force: true do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20140217224223) do
     t.datetime "updated_at"
     t.string   "first_name"
     t.string   "last_name"
+    t.integer  "points"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
