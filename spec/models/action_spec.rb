8b9503678f74cce31aacceb357c088f393d896dd
Juan Carlos <juanjcsr@gmail.com>
2014-02-18 17:40:19 -0600

    agregar pruebas de acciones

--- spec/models/action_spec.rb
+++ spec/models/action_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Action do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before do
+    @action = Action.new(name: "Action 1", desc: "Lorem ipsum", points: 20)
+  end
+
+  subject { @action }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:desc) }
+  it { should respond_to(:level) }
+  it { should respond_to(:points)}
+
+  it { should be_valid }
+
+  describe "when is not created with points" do 
+    before { @action.points = nil}
+    it { should_not be_valid}
+  end
+
 end
