713957af0e482366a17cf5cc84a318aac2951261
Juan Carlos <juanjcsr@gmail.com>
2014-02-24 19:11:12 -0600

    cambiar campo 'desc' de Acciones a tipo texto

--- db/migrate/20140218224306_create_actions.rb
+++ db/migrate/20140218224306_create_actions.rb
@@ -2,7 +2,7 @@ class CreateActions < ActiveRecord::Migration
   def change
     create_table :actions do |t|
       t.string :name
-      t.string :desc
+      t.text :desc
       t.integer :points
 
       t.timestamps
