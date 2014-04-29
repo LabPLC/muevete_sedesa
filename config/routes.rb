07c40e6f0a0012f976c70499aabe6865d2c73d1a
Ryan Closner <ryan@ryanclosner.com>
2014-04-29 17:15:28 -0500

    This commit adds support for engine-light
    Engine-light expects a route of the form '/.well-known/status' to
    return a json object containing details about whether or not the
    application's dependencies are fully functioning.
    
    This commit adds support for engine-light.

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ SedesaApp::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  get "/.well-known/status" => "status#check"
+
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
 
