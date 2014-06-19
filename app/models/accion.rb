24481a4ac1dc266d841a36ef3fe69428aaab146a
Juan Carlos <juanjcsr@gmail.com>
2014-06-18 20:49:32 -0500

    queries bonitos para produccion

--- app/models/accion.rb
+++ app/models/accion.rb
@@ -28,7 +28,7 @@ class Accion< ActiveRecord::Base
   validates :code, :presence => true,
                    :if       => :codigo_valido?
 
-  scope :from_today, lambda {|start_date, end_date| where('fecha_inicio >= ? AND fecha_inicio <= ? OR recurrente == ?', start_date, end_date, "TRUE")}
+  scope :from_today, lambda {|start_date, end_date| where('fecha_inicio >= ? AND fecha_inicio <= ? OR recurrente = ?', start_date, end_date, true)}
 
   #before_ :check_code
 
